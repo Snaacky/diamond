7be6cdde1caa5a93e9a4558dbe5f9a3c87eb947f
Snaacky <snaacky@gmail.com>
2018-06-04 01:52:03 -0400

    Improved comments.

--- diamond.py
+++ diamond.py
@@ -13,24 +13,24 @@ def main():
     print("Diamond has launched.")
 
     while True:
-        if pm.read_int(client + dwEntityList) > 0:
+        if pm.read_int(client + dwEntityList) > 0: # Check if in-game.
             glow_manager = pm.read_int(client + dwGlowObjectManager)
 
-            for i in range(1, 32): # entities 1-32 are reserved for players. 
+            for i in range(1, 32): # Entities 1-32 are reserved for players. 
                 entity = pm.read_int(client + dwEntityList + i * 0x10)
 
                 if entity:
                     entity_team_id = pm.read_int(entity + m_iTeamNum)
                     entity_glow = pm.read_int(entity + m_iGlowIndex)
 
-                    if entity_team_id == 2:
+                    if entity_team_id == 2: # Terrorist
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))  # R 
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))  # G
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))  # B
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1)) # Alpha
                         pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1) # Enable glow
                         
-                    elif entity_team_id == 3:
+                    elif entity_team_id == 3: # Counter-terrorist
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))  # R
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))  # G
                         pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))  # B
