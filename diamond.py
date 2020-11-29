427090a0d99e9e0a6282d52b720d2073f9569e9e
davidgfb <davidgracia99@gmail.com>
2020-11-29 21:32:48 +0100

    use @Slyyxp pull request to update variables and optimize code
    Thank you @Slyyxp for the great support

--- diamond.py
+++ diamond.py
@@ -1,41 +1,46 @@
-import pymem
-import pymem.process
+from pymem import Pymem,process,exception
+import requests
 
-dwEntityList = (0x4D4B104)
-dwGlowObjectManager = (0x5292F20)
-m_iGlowIndex = (0xA428)
-m_iTeamNum = (0xF4)
+try:
+    r = requests.get("https://raw.githubusercontent.com/frk1/hazedumper/master/csgo.json").json()
+    offsets = {"dwEntityList": r['signatures']['dwEntityList'],
+               "dwGlowObjectManager": r['signatures']['dwGlowObjectManager'],
+               "m_iGlowIndex": r['netvars']['m_iGlowIndex'],
+               "m_iTeamNum": r['netvars']['m_iTeamNum']
+              }
 
+    processName='csgo.exe'
+    pm = Pymem(processName)
+    client = process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
-def main():
     print("Diamond has launched.")
-    pm = pymem.Pymem("csgo.exe")
-    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
 
-    while True:
-        glow_manager = pm.read_int(client + dwGlowObjectManager)
+    abierto=True
+    while abierto:
+        glow_manager = pm.read_int(client + offsets['dwGlowObjectManager'])
 
         for i in range(1, 32):  # Entities 1-32 are reserved for players.
-            entity = pm.read_int(client + dwEntityList + i * 0x10)
+            entity = pm.read_int(client + offsets['dwEntityList'] + i * 0x10)
 
-            if entity:
-                entity_team_id = pm.read_int(entity + m_iTeamNum)
-                entity_glow = pm.read_int(entity + m_iGlowIndex)
+            if entity: 
+                entity_team_id = pm.read_int(entity + offsets['m_iTeamNum'])
+                entity_glow = pm.read_int(entity + offsets['m_iGlowIndex'])
 
-                if entity_team_id == 2:  # Terrorist
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(1))   # R 
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(0))   # B
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
-                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+                gMPEGM38=glow_manager + entity_glow * 0x38
 
-                elif entity_team_id == 3:  # Counter-terrorist
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x4, float(0))   # R
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x8, float(0))   # G
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0xC, float(1))   # B
-                    pm.write_float(glow_manager + entity_glow * 0x38 + 0x10, float(1))  # Alpha
-                    pm.write_int(glow_manager + entity_glow * 0x38 + 0x24, 1)           # Enable glow
+                row=entity_team_id-2
+                colores=[[1.0,0.0,0.0,1.0], # Terrorist
+                         [0.0,0.0,1.0,1.0]] # Counter-terrorist
+                pm.write_float(gMPEGM38 + 0x4, colores[row][0])   # R 
+                pm.write_float(gMPEGM38 + 0x8, colores[row][1])   # G
+                pm.write_float(gMPEGM38 + 0xC, colores[row][2])   # B
+                pm.write_float(gMPEGM38 + 0x10, colores[row][3])  # Alpha
 
+                pm.write_int(gMPEGM38 + 0x24, 1)           # Enable glow
 
-if __name__ == '__main__':
-    main()
+except exception.ProcessNotFound:
+    print("error: couldn't find process",processName)
+
+        
+        
+        
